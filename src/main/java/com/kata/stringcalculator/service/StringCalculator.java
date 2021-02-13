40af691b067aab457389850d13472d6121cb9787
abhinav2091 <abhinavsharma043@gmail.com>
2021-02-13 21:24:48 +0530

    for negative numbers

--- src/main/java/com/kata/stringcalculator/service/StringCalculator.java
+++ src/main/java/com/kata/stringcalculator/service/StringCalculator.java
@@ -2,12 +2,14 @@ package com.kata.stringcalculator.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class StringCalculator {
 
-    public int Add(String numbers)
-    {
+    public int Add(String numbers) throws Exception {
+        List<String> negativeNumbers = new ArrayList<>();
         String delimiter=",";
         int result=0;
         if(!StringUtils.hasLength(numbers))
@@ -24,10 +26,17 @@ public class StringCalculator {
                 System.out.println(numbers);
                 String[] numbersArray = numbers.split(delimiter);
                 for (String num : numbersArray) {
+                    if(Integer.parseInt(num)<0)
+                    {
+                        negativeNumbers.add(num);
+                    }
                     result = result + Integer.parseInt(num);
                 }
 
         }
+        if(!negativeNumbers.isEmpty())
+            throw new Exception("negatives not allowed -"+negativeNumbers);
+        else
         return result;
     }
 }
--- src/test/java/com/kata/stringcalculator/StringcalculatorApplicationTests.java
+++ src/test/java/com/kata/stringcalculator/StringcalculatorApplicationTests.java
@@ -2,45 +2,55 @@ package com.kata.stringcalculator;
 
 import com.kata.stringcalculator.service.StringCalculator;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Assert;
 @SpringBootTest
 class StringcalculatorApplicationTests {
 
+	private static  final Logger LOGGER = LoggerFactory.getLogger(StringcalculatorApplicationTests.class);
+
 	@Autowired
 	private StringCalculator stringCalculator;
 	@Test
-	void forEmptyString()
-	{  int result=stringCalculator.Add("");
+	void forEmptyString() throws Exception {  int result=stringCalculator.Add("");
 		Assert.assertEquals(result,0);
 
 	}
 	@Test
-	void forOneValueInString()
-	{  int result=stringCalculator.Add("3");
+	void forOneValueInString() throws Exception {  int result=stringCalculator.Add("3");
 		Assert.assertEquals(result,3);
 
 	}
 	@Test
-	void forMoreThanOneValueInString()
-	{  int result=stringCalculator.Add("2,3,4");
+	void forMoreThanOneValueInString() throws Exception {  int result=stringCalculator.Add("2,3,4");
 		Assert.assertEquals(result,9);
 
 	}
 
 	@Test
-	void forMoreThanNewLineInString()
-	{  int result=stringCalculator.Add("2\n3,4");
+	void forMoreThanNewLineInString() throws Exception {  int result=stringCalculator.Add("2\n3,4");
 		Assert.assertEquals(result,9);
 
 	}
 
 	@Test
-	void forDelimitersString()
-	{  int result=stringCalculator.Add("//;\n1;2");
+	void forDelimitersString() throws Exception {  int result=stringCalculator.Add("//;\n1;2");
 		Assert.assertEquals(result,3);
 
+	}
+
+	@Test
+	void forNegativeNumberString() throws Exception {  int result=stringCalculator.Add("//;\n1;2");
+	try {
+		stringCalculator.Add("-3,-4");
+	}
+	catch (Exception e)
+	{
+		LOGGER.error("{}",e.getMessage());
+	}
 	}
 
 }
